5eb3d8c524c4dd27e1ba4bc971d3febc8385266f
ashutoshhans611 <ashutosh.hans@gmail.com>
2020-04-29 17:37:59 +0530

    add worker and use createIamgeBitmap instead of fileReader

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const $input = document.getElementById('input');
 const $preview = document.getElementById('preview');
 const previewCtx = $preview.getContext('2d');
-
 const fileReader = new FileReader();
 const image = new Image();
+const worker = new Worker('worker.js');
 
 function applyFilter() {
-    const imageData = previewCtx.getImageData(0, 0, image.width, image.height);
-    for (let x = 0; x < image.width; x++) {
-        for (let y = 0; y < image.height; y++) {
-             let index = (x + (y * image.width)) * 4;
-             imageData.data[index] *= 1.2; // make the image more red
-             imageData.data[index + 3] = 127; // change opacity
-        }
-    }
-    previewCtx.putImageData(imageData, 0, 0);
+    const imageData = previewCtx.getImageData(0, 0, $preview.width, $preview.height);
+    worker.postMessage(imageData, [imageData.data.buffer]);
 }
 
-image.addEventListener('load', (e) => {
-    $preview.width = image.width;
-    $preview.height = image.height;
-    previewCtx.drawImage(image, 0, 0);
-    applyFilter();
-});
-
-fileReader.addEventListener('load', (e) => {
-    const base64 = e.target.result;
-    image.src = base64;
+worker.addEventListener('message', (d) => {
+    const imageData = d.data;
+    previewCtx.putImageData(imageData, 0, 0);
 });
 
 $input.addEventListener('change', (e) => {
     const file = e.target.files[0];
-    fileReader.readAsDataURL(file);
+    createImageBitmap(file)
+        .then((bitmap) => {
+            $preview.width = bitmap.width;
+            $preview.height = bitmap.height;
+            previewCtx.drawImage(bitmap, 0, 0);
+            applyFilter();
+        });
 });
